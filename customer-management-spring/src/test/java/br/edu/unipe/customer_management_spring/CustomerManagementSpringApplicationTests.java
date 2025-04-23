1858cd5978c55f3735436d00f273e657843a8f1a
unknown <jpbusinesss@hotmail.com>
2025-04-23 18:29:30 -0300

    first commit

---
+++ customer-management-spring/src/test/java/br/edu/unipe/customer_management_spring/CustomerManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unipe.customer_management_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
