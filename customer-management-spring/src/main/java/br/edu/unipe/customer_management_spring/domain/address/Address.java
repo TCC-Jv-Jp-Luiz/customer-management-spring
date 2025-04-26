4589c356203876e153628fd6890314b790e0c564
Luiz Filipe <luizfilipemr17@gmail.com>
2025-04-26 12:23:08 -0300

    feat: add Address domain

---
+++ customer-management-spring/src/main/java/br/edu/unipe/customer_management_spring/domain/address/Address.java
@@ -0,0 +1,37 @@
+package br.edu.unipe.customer_management_spring.domain.address;
+
+import br.edu.unipe.customer_management_spring.enums.State;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "public_id", unique = true, nullable = false)
+    private String publicId;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "complement")
+    private String complement;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "state", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @Column(name = "postalCode", nullable = false)
+    private String postalCode;
+}
---
+++ customer-management-spring/src/main/java/br/edu/unipe/customer_management_spring/enums/State.java
@@ -0,0 +1,36 @@
+package br.edu.unipe.customer_management_spring.enums;
+
+public enum State {
+
+    ACRE("AC"),
+    ALAGOAS("AL"),
+    AMAPA("AP"),
+    AMAZONAS("AM"),
+    BAHIA("BA"),
+    CEARA("CE"),
+    DISTRITO_FEDERAL("DF"),
+    ESPIRITO_SANTO("ES"),
+    GOIAS("GO"),
+    MARANHAO("MA"),
+    MATO_GROSSO("MT"),
+    MATO_GROSSO_DO_SUL("MS"),
+    MINAS_GERAIS("MG"),
+    PAR("PA"),
+    PARAIBA("PB"),
+    PARANA("PR"),
+    PERNAMBUCO("PE"),
+    PIAUI("PI"),
+    RIO_DE_JANEIRO("RJ"),
+    RIO_GRANDE_DO_NORTE("RN"),
+    RIO_GRANDE_DO_SUL("RS"),
+    RONDONIA("RO"),
+    RORAIMA("RR"),
+    SANTA_CATARINA("SC"),
+    SAO_PAULO("SP"),
+    SERGIPE("SE"),
+    TOCANTINS("TO");
+
+    State(String abbreviation) {
+    }
+
+}
