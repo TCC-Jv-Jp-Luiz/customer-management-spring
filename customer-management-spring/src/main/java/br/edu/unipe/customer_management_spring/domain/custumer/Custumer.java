b4dd844adcf4b8f553e2ff5e0cb00dd077e99b66
unknown <jpbusinesss@hotmail.com>
2025-04-26 13:03:34 -0300

    table cusutmer

---
+++ customer-management-spring/src/main/java/br/edu/unipe/customer_management_spring/domain/custumer/Custumer.java
@@ -0,0 +1,58 @@
+package br.edu.unipe.customer_management_spring.domain.custumer;
+import java.time.LocalDate;
+import br.edu.unipe.customer_management_spring.domain.address.Address;
+import br.edu.unipe.customer_management_spring.domain.auditloginfo.AuditLogInfo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Custumer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "public_id", unique = true, nullable = false)
+    private String publicId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone", unique = true, nullable = false)
+    private String cellPhone;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "cpf", unique = true, nullable = false)
+    private String cpf;
+
+    @Column(name = "birthDate", nullable = false)
+    private LocalDate birthDate;
+
+    @OneToOne
+    @JoinColumn(name = "audit_log_info_id")
+    private AuditLogInfo auditLogInfo;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+}
+    
+
+
+    
