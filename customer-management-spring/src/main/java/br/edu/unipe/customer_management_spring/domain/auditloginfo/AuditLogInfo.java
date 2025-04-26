76832aee561e30bfc610e0b15c084f8b507bfb73
Luiz Filipe <luizfilipemr17@gmail.com>
2025-04-26 12:14:38 -0300

    feat: add AuditLogInfo domain

---
+++ customer-management-spring/src/main/java/br/edu/unipe/customer_management_spring/domain/auditloginfo/AuditLogInfo.java
@@ -0,0 +1,24 @@
+package br.edu.unipe.customer_management_spring.domain.auditloginfo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class AuditLogInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+}
